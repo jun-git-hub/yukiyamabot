67d5ab2ae15dda8a69e3ab9a08397d63266f78b5
jun-git-hub <output1192jun@gmail.com>
2021-01-16 15:43:55 +0900

    make it better

--- scrape.py
+++ scrape.py
@@ -13,14 +13,14 @@ def getSnow():
     soup = BeautifulSoup(html_doc, 'html.parser') # BeautifulSoupの初期化
     snow = soup.find_all("dd",{"class": "greport__data__content"})
 
-    snow_math = ','.join(snow)
     print("手稲スキー場の")
-    print("24時間積雪量:" + snow_math[0])
-    print("山頂の総積雪量:" + snow_math[1])
-    print("山麓の総積雪量:" + snow_math[2])
-    print("雪質:" + snow_math[3])
+    print("24時間積雪量:" + snow[0].text)
+    print("山頂の総積雪量:" + snow[1].text)
+    print("山麓の総積雪量:" + snow[2].text)
+    print("雪質:" + snow[3].text)
 
 getSnow()    
+
 # tags = soup.find_all("dd",{"class": "greport__data__content"})
 
 #print(tags)
