5e62a74e04cd2004b7df7edc7377e4a9dc78282d
jun-git-hub <output1192jun@gmail.com>
2021-01-16 17:24:24 +0900

    make it better

--- main.py
+++ main.py
@@ -61,3 +61,8 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=sc.getSnow())
     )
+
+if __name__ == "__main__" :
+#    app.run()
+    por = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=por)
