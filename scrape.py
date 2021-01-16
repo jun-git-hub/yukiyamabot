18e9b09289abfb4532749cc349e0ae7c42ee4d71
jun-git-hub <output1192jun@gmail.com>
2021-01-16 15:21:56 +0900

    make it better

--- scrape.py
+++ scrape.py
@@ -9,19 +9,16 @@ def getSnow():
     #HTTPリクエスト
     r = requests.get(url)
 
-    html_doc = requests.get(url).text
-    soup = BeautifulSoup(html_doc, 'html.parser') # BeautifulSoupの初期化
+html_doc = requests.get(url).text
+soup = BeautifulSoup(html_doc, 'html.parser') # BeautifulSoupの初期化
+snow = soup.find_all("dd",{"class": "greport__data__content"})
 
-    print("手稲スキー場の")
-    print("24時間積雪量")
-    print("山頂の総積雪量")
-    print("山麓の総積雪量")
-    print("雪質")
-    print("----------------")
-
-    for element in soup.find_all("dd",{"class": "greport__data__content"}):
-        print(element.text)
-
+print("手稲スキー場の")
+print("24時間積雪量:" + snow[0])
+print("山頂の総積雪量:" + snow[1])
+print("山麓の総積雪量:" + snow[2])
+print("雪質:" + snow[3])
+    
 # tags = soup.find_all("dd",{"class": "greport__data__content"})
 
 #print(tags)
