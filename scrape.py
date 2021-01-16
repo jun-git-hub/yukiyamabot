89b13517f471541b39bdb786925a8a0a6da17994
jun-git-hub <output1192jun@gmail.com>
2021-01-16 15:05:17 +0900

    make it better

--- scrape.py
+++ scrape.py
@@ -3,23 +3,24 @@ import urllib.request
 import json
 import requests
 
-url = 'https://sapporo-teine.com/snow/gelande-report'
-ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) '\
-     'AppleWebKit/537.36 (KHTML, like Gecko) '\
-     'Chrome/67.0.3396.99 Safari/537.36 '
+def getSnow():
+    #手稲スキー場のゲレンデレポートのページ
+    url = 'https://sapporo-teine.com/snow/gelande-report'
+    #HTTPリクエスト
+    r = requests.get(url)
 
-html_doc = requests.get(url).text
-soup = BeautifulSoup(html_doc, 'html.parser') # BeautifulSoupの初期化
+    html_doc = requests.get(url).text
+    soup = BeautifulSoup(html_doc, 'html.parser') # BeautifulSoupの初期化
 
-print("手稲スキー場の")
-print("24時間積雪量")
-print("山頂の総積雪量")
-print("山麓の総積雪量")
-print("雪質")
-print("----------------")
+    print("手稲スキー場の")
+    print("24時間積雪量")
+    print("山頂の総積雪量")
+    print("山麓の総積雪量")
+    print("雪質")
+    print("----------------")
 
-for element in soup.find_all("dd",{"class": "greport__data__content"}):
-    print(element.text)
+    for element in soup.find_all("dd",{"class": "greport__data__content"}):
+        print(element.text)
 
 # tags = soup.find_all("dd",{"class": "greport__data__content"})
 
