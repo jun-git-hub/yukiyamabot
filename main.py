1ee68c7a73a23d6a51a3978a7686d8fa53537851
jun-git-hub <output1192jun@gmail.com>
2021-01-17 00:16:05 +0900

    2

--- main.py
+++ main.py
@@ -64,5 +64,5 @@ def handle_message(event):
 
 if __name__ == "__main__" :
 #    app.run()
-    por = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=por)
+    port = int(os.getenv("PORT"))
+    app.run(host="0.0.0.0", port=port)
