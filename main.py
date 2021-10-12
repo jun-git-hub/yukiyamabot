609a16432816e797ee6d1f7e8eaf4dfadf6f7364
岡部純 <okabejun@okabejunnoMacBook-Air.local>
2021-10-12 16:09:34 +0900

    test

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    result = scrapeTeine.getSnow()
+    result = scrape.getSnow()
 
     line_bot_api.reply_message(
         event.reply_token,
