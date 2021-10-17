4a37f9d9809605d707f9493f0218e8067418e943
岡部純 <okabejun@okabejunnoMacBook-Air.local>
2021-10-17 23:30:56 +0900

    request_message

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == '手稲':
+    request_message = event.message.text
+    if request_message == '手稲':
         result = scrape.getSnow()
     else :
         result = 'もう一度'
@@ -42,7 +43,6 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=result))
-
 
 if __name__ == "__main__":
 #    app.run()
