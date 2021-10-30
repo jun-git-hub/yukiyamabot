ffbdcd487d8e11875523736cda1300c1e1908617
岡部純 <okabejun@okabejunnoMacBook-Air.local>
2021-10-30 16:47:02 +0900

    get

--- weatherfore.py
+++ weatherfore.py
@@ -6,19 +6,19 @@ API_KEY = 'f29b3ea5029bdb87545aa8fa70d6e298'
 def getWeather(LON, LAN):
     data = requests.get(
         "https://api.openweathermap.org/data/2.5/onecall",
-        {},
-        {
-            "appid":API_KEY,
-            "lon":LON,
-            "lat":LAN,
-            "units":"metric",
-            "lang":"ja",
-            "exculde":"current,minutely,daily,alerts"
+        headers={},
+        params={
+        "appid":API_KEY,
+        "lon":LON,
+        "lat":LAN,
+        "units":"metric",
+        "lang":"ja",
+        "exculde":"current,minutely,daily,alerts"
         }
     )
     jsondata = data.json()
     
     for hour in jsondata["hourly"]:
-        weather = "-------------------------/n" + datetime.datetime.fromtimestamp(hour["dt"]) + "/n" + hour["weather"][0]["description"] + "/n"
+        weather = "-------------------------/n" + str(datetime.datetime.fromtimestamp(hour["dt"])) + "/n" + hour["weather"][0]["description"] + "/n"
     
     return weather
